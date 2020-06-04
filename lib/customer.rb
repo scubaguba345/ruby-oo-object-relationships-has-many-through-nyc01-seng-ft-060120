56254b0011b8096d812236e470c919fc5ad4e666
scubaguba345 <brandon.gubitosa@gmail.com>
2020-06-04 17:29:33 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,33 @@
 class Customer
+
+attr_accessor :name, :age
+
+@@all = []
+
+def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+end
+
+def self.all
+    @@all
+end
+
+def new_meal(waiter, total, tip=0) #creates new meal as customer and automatically associate each meal with the customer that created it
+    Meal.new(waiter, self, total, tip) #passing self as a reference to current instance of customer
+end
+
+def meals #customer looks at all the meals then selects one that belongs to them
+    Meal.all.select do |meal| #iterate through every instance of meal 
+        meal.customer == self  #returning ones where the meals customer matches the current customer instance
+    end
+end
+
+def waiters
+    meals.map do |meal|
+        meal.waiter
+    end
+end
   
 end
